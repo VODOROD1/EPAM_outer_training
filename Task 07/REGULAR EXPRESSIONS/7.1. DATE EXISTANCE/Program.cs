ab218e21cd2009a1c3bffe0326911a8b9746efa5
VODOROD1 <vodorodo51@gmail.com>
2019-09-06 16:52:40 +0400

    Added 7.1 7.2

---
+++ Task 07/REGULAR EXPRESSIONS/7.1. DATE EXISTANCE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _7._1.DATE_EXISTANCE
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст, содержащий дату в формате dd-mm-yyyy: '");
+
+            var inputString = Console.ReadLine();
+            //регулярное выражение поиска даты
+            var logic = Regex.IsMatch(inputString, @"((0[1-9]|[1-2]\d|3[0-1])-(0[1-9]|1[0-2])-\d{4})");
+
+            if (logic)
+            {
+                Console.WriteLine($"В тексте '{inputString}' содержится дата!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"В тексте '{inputString}' НЕ содержится дата!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Task 07/REGULAR EXPRESSIONS/7.2. HTML REPLACER/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _7._2.HTML_REPLACER
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите текст:");
+            var inputText = Console.ReadLine();
+            Console.WriteLine();
+            var replacedText = Regex.Replace(inputText, @"<[^<>]+>", "_");
+            Console.WriteLine($"Результат замены {replacedText}");
+            Console.ReadKey();
+        }
+    }
+}
